9d7ef045c1b7d3c3e31dc369cf86c07e156c4e38
Viresh_Jadhav <168194192+vireshjadhav@users.noreply.github.com>
2025-06-05 02:41:46 +0530

    Add collectible key item with score increment on pickup

---
+++ Assets/Scripts/KeyController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyController : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+         if(collision.gameObject.GetComponent<PlayerController>() != null)
+        {
+            PlayerController playerContoller = collision.gameObject.GetComponent<PlayerController>();
+            playerContoller.PickUpKey();
+            animator.SetTrigger("KeyCollected");
+
+            StartCoroutine(DestroyAfterDelay(1f));
+
+        }
+
+        Collider2D col = GetComponent<Collider2D>();
+        col.enabled = false;
+
+    }
+
+    private IEnumerator DestroyAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(gameObject);
+    }
+
+}
